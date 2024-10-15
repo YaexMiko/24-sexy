5214da333d61a0f5163721d9420aa128c4ec0fef
Yae Miko <164617880+Miko756@users.noreply.github.com>
2024-10-15 20:12:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -142,12 +142,14 @@ async def start_command(client: Client, message: Message):
             if IS_VERIFY and not verify_status['is_verified']:
                 short_url = f"api.shareus.io"
                 full_tut_url = f"https://t.me/neprosz/3"
+                text = f"https://t.me/neprosz/3"
                 token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
                 await update_verify_status(id, verify_token=token, link="")
                 link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
                 btn = [
                     [InlineKeyboardButton("Click here", url=link)],
-                    [InlineKeyboardButton('How to use the bot', url=full_tut_url)]
+                    [InlineKeyboardButton('How to use the bot', url=full_tut_url)],
+                    [InlineKeyboardButton('How to use the bot', callback_data=text)]
                 ]
                 await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 Hour after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
 
